fbbdcdd85c92f207cf55374582d1e2ede7a9b4ea
MokshaVemula <moksha.vemula@xcubelabs.com>
2022-07-05 17:24:46 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import RootNavigator from "./src/navigation/RootNavigator";
+
+const App = () =>{
+  return<RootNavigator />;
+}
+export default App;
---
+++ src/components/Separator.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View} from 'react-native';
+import { exp } from "react-native/Libraries/Animated/Easing";
+
+const Separator = () =>(
+    <View 
+      style={{borderBottomColor:'#d3d3d3', borderBottomWidth:1, marginTop:10, marginBottom:10}}
+    />
+);
+
+export default Separator
